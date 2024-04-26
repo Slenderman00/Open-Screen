2367df5ab5a3d80db32f97d70b02581bb9465239
Joar Heimonen <joarheimonen@live.no>
2024-04-26 10:07:09 +0200

    feat: added mediapipe pose estimation to find treshold depth

--- main.py
+++ main.py
@@ -3,15 +3,26 @@ import os
 from PIL import Image
 import cv2
 import numpy as np
+import mediapipe as mp
+import math
 
 # Set environment variable
 os.environ['QT_QPA_PLATFORM'] = 'xcb'
+
+# pose
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=False)
 
 # Load depth estimation model
 checkpoint = "vinvino02/glpn-nyu"
 depth_estimator = pipeline("depth-estimation", model=checkpoint)
 
 vid = cv2.VideoCapture(0)
+
+width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+small_width = math.floor(width * 0.25)
+small_height = math.floor(height * 0.25)
 
 while True:
     # Capture the video frame
@@ -24,11 +35,30 @@ while True:
     predictions = depth_estimator(pil_image)
 
     depth_map = predictions["depth"]
-    threshold_depth = np.percentile(depth_map, 25)
+
+    pose_res = pose.process(original_frame)
+    depth_values = []
+    for landmark in pose_res.pose_landmarks.landmark:
+        x = math.floor(landmark.x * small_width)
+        y = math.floor(landmark.y * small_height)
+
+        print(depth_map.size)
+
+        if 0 <= x < small_width and 0 <= y < small_height:
+            print(x, y, small_width, small_height)
+
+            depth_value = depth_map.getpixel((x, y))
+            depth_values.append(depth_value)
+
+    threshold_depth = np.average(depth_values) + 5
+    print({'treshold_depth': threshold_depth})
     binary_mask = np.where(depth_map <= threshold_depth, 255, 0).astype(np.uint8)
 
     binary_mask_resized = cv2.resize(binary_mask, (original_frame.shape[1], original_frame.shape[0]))
     masked_frame = cv2.bitwise_and(original_frame, original_frame, mask=binary_mask_resized)
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing.draw_landmarks(masked_frame, pose_res.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
     cv2.imshow('masked frame', masked_frame)
 
